af264a5c5356c253b56e5de45b24af14f8af9e3f
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2023-02-08 13:58:40 +0530

    kfhuaishfiudsahifhisdaufhiudas

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -3,41 +3,7 @@ import React from 'react'
 export default function Login() {
     return (
         <div>
-            <div style={{ marginTop: "150px" }} >
-                <div class="row" style={{ display: 'flex', justifyContent: 'center', width: "110%", height: "60%" }}>
-                    <div class="col-sm-3">
-                        <div class="card">
-                            <div class="card-header" >
-                                Login Form
-                            </div>
-
-                            <div class="card-body">
-                                <form>
-                                    <div class="card text-bg-light mb-3">
-                                        <label for="exampleInputEmail1" class="form-label, fw-light">Email address</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="exampleInputPassword1" class="form-label, fw-light">Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1" />
-                                    </div>
-                                    <div>
-                                        <button class="btn btn-primary">Login</button>
-                                    </div>
-
-                                </form>
-                            </div>
-
-                            <div class="card-footer">
-
-                                <button class="btn btn-primary">New User</button>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            <h1>I love INDIA</h1>
         </div>
     )
 }
