db2c62668a4852ed05bc05bde2f8f418ef69fdd1
Utkarsh Tripathi <utkarsh.ilh@gmail.com>
2023-02-08 13:56:18 +0530

    a short description of the change

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
     return (
         <div>
-            <h1>This is Home page</h1>
+            <h1>This is Homee</h1>
 
         </div>
     )
